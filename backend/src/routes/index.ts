708a982d22942b9cd9834aa309b4f4625029cb49
Arnaud Méron <arnaud.meron2004@gmail.com>
2025-06-25 10:35:39 +0200

    added and tested hospital routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono';
 import { authRouter } from './auth.routes';
 import { deliveryRouter } from './delivery';
+import { hospitalRouter } from './hospital';
 import { swaggerUI } from '@hono/swagger-ui';
 import { createRoute, z } from '@hono/zod-openapi';
 
@@ -99,7 +100,7 @@ export const createRouter = () => {
   // Routes publiques
   api.route('/auth', authRouter);
   api.route('/deliveries', deliveryRouter);
-  
+  api.route('/hospitals', hospitalRouter);
 
   // Swagger documentation
   api.get('/docs', (c) => c.json(openApiJson));
