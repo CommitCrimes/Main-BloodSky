e93dc3f997954f363b9734789f00c607824acbf1
alexfrai <alex.fraioli.af@gmail.com>
2025-06-25 09:47:33 +0200

    Recup des routes du back pour integration dans le front

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -2,23 +2,27 @@ import React, { useState } from 'react'
 
 const SearchBar = () => {
    const [input, setInput] = useState();
+   const [searchResult, setSearchResult] = useState("");
 
-   const filterSearchedContent = () => {
-
+   const filterSearchedContent = (filterParam) => {
+    // orderBy filterParam
+    const filteredSearch = ""
+    setSearchResult(filteredSearch)
    }
 
   const submitSearch = (e : React.FormEvent) => {
     e.preventDefault();
     console.log(input)
     console.log(e)
-    //requette api pour recup les data
-    //recup res
-    //classe via le filtre et la disposition (A a Z)
-
+    
+    // requette api pour recup les data
+    // recup res
+    // classe via le filtre
+    // filterSearchedContent()
   }
   return (
       <form className='w-full' onSubmit={(searchedInput)=>{submitSearch(searchedInput)}}>
-        <div className='flex flex-col items-center w-full p-2 border-2 border-solid border-black rounded-xl '>
+        <div className='flex flex-col items-center w-full p-2 border-2 border-solid border-black rounded-xl relative'>
             <input type="text" className='flex border-black border-2 rounded-full p-2 w-4/6' onChange={(e) => setInput(e.target.value)}/>
             
             <div>
@@ -39,15 +43,15 @@ const SearchBar = () => {
 
             </div>
 
-            <div>
+            <div className='absolute right-0'>
                 <select name="filterOption" id="filterOption">
                     <option value="">Option de filtrage</option>
-                    <option value="dog">A a Z</option>
-                    <option value="cat">Z a A</option>
-                    <option value="hamster">numero de livraison croissant</option>
-                    <option value="parrot">numero de livraison croissant</option>
-                    <option value="spider">Date Recent / Vieux</option>
-                    <option value="goldfish">Goldfish</option>
+                    <option value="AaZ">A a Z</option>
+                    <option value="ZaA">Z a A</option>
+                    <option value="croissant">numero de livraison croissant</option>
+                    <option value="decroissant">numero de livraison croissant</option>
+                    <option value="new">Date Recent / Vieux</option>
+                    <option value="old">Date Vieux / Recent</option>
                 </select>
             </div>
 
