7927f983490dba0cd5ce6b39142a5e6c3a95e3f7
Arnaud Méron <arnaud.meron2004@gmail.com>
2025-06-26 15:45:20 +0200

    fix merge bug

--- backend/src/schemas/user.ts
+++ backend/src/schemas/user.ts
@@ -7,10 +7,6 @@ export const users = pgTable('users', {
   userName: varchar('user_name'),
   userFirstname: varchar('user_firstname'),
   dteCreate: timestamp('dte_create').defaultNow(),
-<<<<<<< 4-créer-les-routes-crud-de-la-table-user
-  telNumber: varchar('tel_number'),
-  userStatus: text('user_status'), // suspended or active
-=======
   telNumber: integer('tel_number'),
   userStatus: text('user_status'), // suspended, active, pending
   tempPasswordToken: varchar('temp_password_token'),
@@ -18,5 +14,4 @@ export const users = pgTable('users', {
   urlUsed: boolean('url_used').default(false),
   resetPasswordToken: varchar('reset_password_token'),
   resetPasswordExpires: timestamp('reset_password_expires'),
->>>>>>> All-crud
 });
