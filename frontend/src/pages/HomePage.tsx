797a7d392184e9f890671f6e939cafa8a353b41e
belli <bellinna.uong@outlook.com>
2025-07-08 15:54:25 +0200

    Add icon with right size

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -4,6 +4,9 @@ import '../Home.css';
 import droneImg from '../assets/drone.png';
 import bloodHomeImg from '../assets/blood-home.png';
 import medicalHomeImg from '../assets/medical-home.png';
+import feature1 from '../assets/icon-home/blood.png';
+import feature2 from '../assets/icon-home/board.png';
+import feature3 from '../assets/icon-home/center.png';
 
 const Section1 = () => (
   <section className="section1 section1-custom" style={{ maxHeight: '100vh', padding: '2rem', position: 'relative', overflow: 'hidden' }}>
@@ -62,14 +65,49 @@ const Section3 = () => (
     </div>
   </section>
 );
-const Section4 = () => <section className="section4" style={{minHeight: '100vh', padding: '2rem' }}>Section 4</section>;
-const Section5 = () => <section className="section5" style={{ minHeight: '30vh', padding: '2rem' }}>Section 5</section>;
+const Section4 = () => (
+  <section className="section4 section4-custom">
+    <h1 className="section4-title">Explorez nos fonctionnalités clés</h1>
+    <div className="section4-content-blur">
+        <div className="section4-features">
+            <div className="section4-feature-card">
+                <img src={feature1} alt="Fonctionnalité 1" className="section4-feature-icon"/>
+                <div className="section4-feature-text">Planification de livraisons</div>
+            </div>
+            <div className="section4-feature-card">
+                <img src={feature2} alt="Fonctionnalité 2" className="section4-feature-icon"/>
+                <div className="section4-feature-text">Traçabilité des poches de sang</div>
+            </div>
+            <div className="section4-feature-card">
+                <img src={feature3} alt="Fonctionnalité 3" className="section4-feature-icon"/>
+                <div className="section4-feature-text"> Demande de
+                    livraison en ligne</div>
+            </div>
+            <div className="section4-feature-card">
+                <img src={feature3} alt="Fonctionnalité 3" className="section4-feature-icon"/>
+                <div className="section4-feature-text">Livraison rapide
+                    par drone</div>
+            </div>
+            <div className="section4-feature-card">
+                <img src={feature3} alt="Fonctionnalité 3" className="section4-feature-icon"/>
+                <div className="section4-feature-text">Suivi en temps réel
+                    des colis</div>
+            </div>
+            <div className="section4-feature-card">
+                <img src={feature3} alt="Fonctionnalité 3" className="section4-feature-icon"/>
+                <div className="section4-feature-text">Sécurité et fiabilité des données</div>
+            </div>
+        </div>
+    </div>
+  </section>
+);
+const Section5 = () => <section className="section5" style={{minHeight: '30vh', padding: '2rem'}}>Section 5</section>;
 
 const HomePage: React.FC = () => {
-  return (
-    <div>
-      <Section1 />
-      <Section2 />
+    return (
+        <div>
+            <Section1/>
+            <Section2 />
       <Section3 />
       <Section4 />
       <Section5 />
